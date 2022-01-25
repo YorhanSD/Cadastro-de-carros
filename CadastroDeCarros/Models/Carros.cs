9736b41d65e4d7e784bfa7fbb4d47ca5907bf626
yorhanSD <yorhansd08@gmail.com>
2022-01-24 23:05:04 -0300

    Initial commit

---
+++ CadastroDeCarros/Models/Carros.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadastroDeCarros.Models
+{
+    [Table("Carros")]
+    public class Carros
+    {
+        [Display(Name = "Id")]
+        [Column("Id")]
+
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+
+        public string Nome { get; set; }
+
+        [Display(Name = "Marca")]
+        [Column("Marca")]
+
+        public string Marca { get; set; }
+
+        [Display(Name = "Ano")]
+        [Column("Ano")]
+
+        public int Ano { get; set; }
+
+        [Display(Name = "Motor")]
+        [Column("Motor")]
+
+        public string Motor { get; set; }
+
+        [Display(Name = "Turbo")]
+        [Column("Turbo")]
+
+        public bool Turbo { get; set; }
+    }
+}
